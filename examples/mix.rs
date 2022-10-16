f2e8dc7b7563c33a1b6ca8fec3b24fff1c9c4d5e
Cas Brugman <casbrugman@hotmail.nl>
2022-10-16 22:35:39 +0200

    Use input for mix

--- examples/mix.rs
+++ examples/mix.rs
@@ -11,7 +11,7 @@ fn main() {
     Player::play(Amp::new(
         0.05,
         Mix((0..10)
-            .map(|_| WaveTable::new_sine(1000, rng.gen_range(1.0..250.0)))
+            .map(|_| WaveTable::new_sine(1000, rng.gen_range(1.0..250.0)).into())
             .collect()),
     ));
 }
--- src/modifiers/mix.rs
+++ src/modifiers/mix.rs
@@ -1,13 +1,13 @@
-use crate::Synth;
+use crate::{input::Input, Synth};
 
-pub struct Mix<T>(pub Vec<T>);
+pub struct Mix(pub Vec<Input>);
 
-impl<T: Synth> Synth for Mix<T> {
+impl Synth for Mix {
     fn get_sample(&mut self, rate: u32, index: u32) -> Option<f32> {
         let mut sample = 0.0;
 
         for synth in self.0.iter_mut() {
-            sample += synth.get_sample(rate, index)?;
+            sample += synth.get_input(rate, index)?;
         }
 
         Some(sample)
