330637d298b003a296989edc01bec957550b8c2a
Cas Brugman <casbrugman@hotmail.nl>
2022-10-18 02:29:54 +0200

    Closure can be FnMut

--- examples/closure.rs
+++ examples/closure.rs
@@ -2,8 +2,17 @@ use std::f32::consts::PI;
 
 use synth::{ocils::Closure, player::Player};
 fn main() {
-    Player::play(
-        Closure::new(|rate, index| Some((index as f32 / rate as f32 * 2.0 * PI * 200.0).sin()))
-            * 0.5,
-    );
+    const FREQ: f32 = 200.0;
+    const VOL: f32 = 0.5;
+
+    let mut index = 0.0;
+    Player::play(Closure::new(move |rate| {
+        let len = 1.0 / rate as f32;
+        index += len * FREQ;
+        index %= 1.0;
+        let angle = index * 2.0 * PI;
+        let ampl = angle.sin() * VOL;
+
+        Some(ampl)
+    }));
 }
