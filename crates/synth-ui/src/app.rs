eabef5a585100c4f4bbaa40eb42cf9b841045a9d
Cas Brugman <casbrugman@hotmail.nl>
2023-02-15 01:09:04 +0100

    Eframe settings

--- crates/synth-ui/src/app.rs
+++ crates/synth-ui/src/app.rs
@@ -10,11 +10,21 @@ impl<T: Ui + 'static> App<T> {
     }
 
     pub fn run(self) {
+        let options = NativeOptions {
+            initial_window_size: Some(egui::vec2(1280.0, 720.0)),
+            centered: true,
+            ..Default::default()
+        };
+
         eframe::run_native(
             self.0.title().to_owned().as_str(),
-            NativeOptions::default(),
-            Box::new(|_| Box::new(self)),
-        ).ok();
+            options,
+            Box::new(|cc| {
+                cc.egui_ctx.set_pixels_per_point(1.5);
+                Box::new(self)
+            }),
+        )
+        .ok();
     }
 }
 
